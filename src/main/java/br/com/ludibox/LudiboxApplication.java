8a78c65113750db4ea384f257ba7e509c67e7ee3
Fernanda Dias <fernandadias9@hotmail.com>
2024-12-10 09:35:44 -0300

    commit inicial

---
+++ src/main/java/br/com/ludibox/LudiboxApplication.java
@@ -0,0 +1,13 @@
+package br.com.ludibox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LudiboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LudiboxApplication.class, args);
+	}
+
+}
