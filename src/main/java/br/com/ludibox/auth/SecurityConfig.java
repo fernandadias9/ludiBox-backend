712250817b2e62fffb96fb3553a717489dbbf32c
Icaleb1 <calebfernandes080@gmail.com>
2025-06-28 12:04:44 -0300

    feat: adicionado rota do render no CORs

--- src/main/java/br/com/ludibox/auth/SecurityConfig.java
+++ src/main/java/br/com/ludibox/auth/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(List.of("http://localhost:4200")); // Libera a origem do Angular
+		configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://ludibox-frontend.onrender.com")); // Libera a origem do Angular
 		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")); // Métodos HTTP permitidos
 		configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Access-Control-Allow-Origin", 
 												"Access-Control-Allow-Headers","Access-Control-Expose-Headers",
