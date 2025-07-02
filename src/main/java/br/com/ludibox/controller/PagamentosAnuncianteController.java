586041fd753412ec062facbe0133bc7e6771adce
EMANUEL-KRAUSS-DE-SOUZA <emanuelkraussdesouza@gmail.com>
2025-07-01 23:35:22 -0300

    Feat: Listagem de pagamentos pendentes + Pagar Anunciante.

--- src/main/java/br/com/ludibox/controller/PagamentosAnuncianteController.java
+++ src/main/java/br/com/ludibox/controller/PagamentosAnuncianteController.java
@@ -1,13 +1,12 @@
 package br.com.ludibox.controller;
 
+import br.com.ludibox.model.dto.FiltroPagamentoDTO;
 import br.com.ludibox.model.entity.PagamentosAnunciante;
 import br.com.ludibox.service.PagamentosAnuncianteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/pagamentos")
@@ -21,4 +20,17 @@ public class PagamentosAnuncianteController {
         PagamentosAnunciante saved = service.salvar(novoPagamento);
         return ResponseEntity.ok(saved);
     }
+
+    @PostMapping("/filtrar")
+    public ResponseEntity<List<PagamentosAnunciante>> listarComFiltro(@RequestBody FiltroPagamentoDTO filtro) {
+        List<PagamentosAnunciante> lista = service.listarComFiltro(filtro);
+        return ResponseEntity.ok(lista);
+    }
+
+    @PutMapping("/pagar/{id}")
+    public ResponseEntity<?> marcarComoPago(@PathVariable Integer id) {
+        service.marcarComoPago(id);
+        return ResponseEntity.ok().build();
+    }
 }
+
