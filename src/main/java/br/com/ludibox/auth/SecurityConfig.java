2173f496770cb933aad634cd3bbd4c70f242f2a2
Fernanda Dias <fernandadias9@hotmail.com>
2025-07-06 17:33:08 -0300

    fix springsecurity

--- src/main/java/br/com/ludibox/auth/SecurityConfig.java
+++ src/main/java/br/com/ludibox/auth/SecurityConfig.java
@@ -53,8 +53,8 @@ public class SecurityConfig {
 				//Hierarquia de permissões e bloqueios
 				auth -> auth
 				//URLs liberadas
-				.requestMatchers("/auth/**", "/public/**", "/produto/listar", "/produto/listarComFiltro", "/produto/buscar/*", "/api/password/reset", "/avaliacoes/produto/**").permitAll()
-				.requestMatchers(HttpMethod.GET, "/produto/listarComFiltro").permitAll()
+				.requestMatchers(HttpMethod.GET, "/produto/listarComFiltro*").permitAll()
+				.requestMatchers("/auth/**", "/public/**", "/produto/listar", "/produto/buscar/*", "/api/password/reset", "/avaliacoes/produto/**").permitAll()
 				//Todas as demais são bloqueadas
 				.anyRequest().authenticated())
 		.httpBasic(Customizer.withDefaults())
